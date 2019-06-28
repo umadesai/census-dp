277aea22bc365dedd2b0e292d0f5740321a010fe
Uma Desai <uma.desai@students.olin.edu>
2019-06-28 16:06:29 -0400

    parameterize test

--- tests/test_noise.py
+++ tests/test_noise.py
@@ -1,18 +1,31 @@
 from .context import census_dp
 from census_dp import noise
+import pytest
 
 
-def test_laplace_mech():
+@pytest.mark.parametrize("mu, epsilon, sensitivity, res", [
+    (0, 2, 1, 0),
+    (1, 4, 1, 1),
+    (2, 1, 1, 2),
+    (3, 5, 1, 3),
+])
+def test_laplace_mech(mu, epsilon, sensitivity, res):
     total = 0
     for i in range(1000):
-        total += noise.laplace_mech(mu=0, epsilon=2, sensitivity=1)
+        total += noise.laplace_mech(mu=mu, epsilon=epsilon, sensitivity=sensitivity)
     k = total/1000.0
-    assert(round(k, 1) == 0)
+    assert(round(k, 1) == res)
 
 
-def test_geometric_mech():
+@pytest.mark.parametrize("mu, budget, res", [
+    (0, 2, 0),
+    (1, 4, 1),
+    (2, 1, 2),
+    (3, 3, 3),
+])
+def test_geometric_mech(mu, budget, res):
     total = 0
     for i in range(1000):
-        total += noise.geometric_mech(0, 1)
+        total += noise.geometric_mech(mu, budget)
     k = total/1000.0
-    assert(round(k, 1) == 0)
+    assert(round(k, 1) == res)
