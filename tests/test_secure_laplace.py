f220a64ab9cef0698e8f82d2ec5b35c4e33c94c4
Uma Desai <uma.desai@students.olin.edu>
2019-07-24 10:49:45 -0400

    add test for secure_laplace_mech

---
+++ tests/test_secure_laplace.py
@@ -0,0 +1,13 @@
+import pytest
+from .context import census_dp
+from census_dp import secure_laplace
+
+
+def test_secure_laplace_mech():
+    query_sensitivity = 0.1
+    privacy_budget = 10
+    total = 0
+    for i in range(1000):
+        total += secure_laplace.secure_laplace_mech(scale=query_sensitivity / privacy_budget)
+    k = total/1000.0
+    assert(k < 0.1 and k > -0.1)
